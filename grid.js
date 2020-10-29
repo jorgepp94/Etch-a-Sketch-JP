4abd8216f9da3c13f3935584f21baadf17372e49
Jorge Perez <jorgealbertopp@hotmail.com>
2020-10-29 11:23:10 -0600

    Added the grid cleaner option

--- grid.js
+++ grid.js
@@ -1,12 +1,14 @@
 const container = document.getElementById("grid-container");
-let newGrid;
-makeRows(16);
+let box;
+
+
+makeRows(25);
 
 function makeRows(gridSize) {
   container.style.setProperty('--grid-rows', gridSize);
   container.style.setProperty('--grid-cols', gridSize);
   for (c = 0; c < (gridSize*gridSize); c++) {
-    let box = document.createElement("div");
+    box = document.createElement("div");
     container.appendChild(box).className = "grid-item";
     box.addEventListener("mouseover", (e) =>{
       e.target.style.backgroundColor = "#48494B";
@@ -14,10 +16,14 @@ function makeRows(gridSize) {
   }
 }
 
-
-
 const cleaner = document.getElementById("cleaner");
 cleaner.addEventListener("click", (e) =>{
-    newGrid = prompt("Enter a new grid size:");
-    makeRows(newGrid);
+    //let newGrid = prompt("Enter a new grid size:");
+    let gridCleaner = document.querySelectorAll(".grid-item");
+    console.log(gridCleaner);
+    gridCleaner.forEach(myFunction);
+
+    function myFunction(gridCleaner){
+      gridCleaner.style.backgroundColor = "transparent";
+    }
 });
