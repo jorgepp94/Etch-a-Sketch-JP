2299215db6acadb8e9b0d79a18591fa7d5f813ea
Jorge Perez <jorgealbertopp@hotmail.com>
2020-11-03 09:34:55 -0600

    Added function to limit the number of squares on the grid

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch-JP
+# Please drag the pointer of the mouse through the grid to draw.
+# In case the squares of the grid are too beg, kindly click "CLEAN" in order to create a new grid with more squares
--- grid.js
+++ grid.js
@@ -1,29 +1,52 @@
-const container = document.getElementById("grid-container");
-let box;
-
-
-makeRows(25);
 
 function makeRows(gridSize) {
+  const container = document.getElementById("grid-container");
+  //textContent = ""; cleans the grid
+  container.textContent = " ";
   container.style.setProperty('--grid-rows', gridSize);
   container.style.setProperty('--grid-cols', gridSize);
   for (c = 0; c < (gridSize*gridSize); c++) {
-    box = document.createElement("div");
-    container.appendChild(box).className = "grid-item";
+    let box = document.createElement("div");
     box.addEventListener("mouseover", (e) =>{
       e.target.style.backgroundColor = "#48494B";
     });
+    container.appendChild(box).className = "grid-item";
   }
 }
 
 const cleaner = document.getElementById("cleaner");
 cleaner.addEventListener("click", (e) =>{
-    //let newGrid = prompt("Enter a new grid size:");
+    
     let gridCleaner = document.querySelectorAll(".grid-item");
     console.log(gridCleaner);
-    gridCleaner.forEach(myFunction);
+    gridCleaner.forEach((cleanItems) =>{
+      cleanItems.style.backgroundColor = "transparent";
+    });
 
-    function myFunction(gridCleaner){
-      gridCleaner.style.backgroundColor = "transparent";
+    let newGrid = limitGrid(prompt("Enter a new grid size:"));
+    if (newGrid){
+      makeRows(newGrid);
     }
+    
 });
+
+makeRows(10);
+
+//Grid size limitation
+
+function limitGrid (gridValue){
+  if (!gridValue) {
+    alert("Please enter a number between 2 and 100");
+    return false;
+}
+    if (gridValue>=100) {
+        alert("Please enter a number smaller than 100");
+        return false;
+    }
+
+    if (gridValue<=1) {
+        alert("Please enter a number bigger than 1");
+        return false;
+    }
+return gridValue;
+}
